3f9e3c182ed30aa50957e94d5ad91a45cd0edd5b
Robert Jonczy <robert.jonczy@gmail.com>
2018-10-27 03:21:35 +0200

    test of python package

--- funniest/__init__.py
+++ funniest/__init__.py
@@ -1,3 +1,6 @@
 def joke():
     return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
             u'Beiherhund das Oder die Flipperwaldt gersput.')
+
+def joke2():
+    return('siema')
